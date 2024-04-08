1b75e68b6ad86435bcd252ceaed8dfac317753f3
sreeharsha-rav <dev.sreeharsha@gmail.com>
2024-04-08 00:09:59 -0700

    add: header component

---
+++ do-connect-frontend/src/app/shared/header/header.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    SidebarComponent
+  ],
+  template: `
+    <!-- Header -->
+    <mat-toolbar>
+      <button mat-icon-button id="sidebarToggle" (click)="toggleSidebar()">
+        @if (isSidebarOpen) {
+          <mat-icon>close</mat-icon>
+        } @else {
+          <mat-icon>menu</mat-icon>
+        }
+      </button>
+      <!-- Title -->
+      <span>DoConnect</span>
+      <span class="example-spacer"></span>
+      <!-- User profile or login/signup -->
+      @if (isUserLoggedIn) {
+        <button mat-icon-button>
+          <mat-icon>account_circle</mat-icon>
+          <span>Profile</span>
+        </button>
+      } @else {
+        <button mat-icon-button>
+          <mat-icon>login</mat-icon>
+          <span>Login</span>
+        </button>
+        <button mat-icon-button>
+          <mat-icon>person_add</mat-icon>
+          <span>Sign Up</span>
+        </button>
+      }
+    </mat-toolbar>
+    <!-- Sidebar -->
+    <app-sidebar></app-sidebar>
+  `,
+  styles: ``
+})
+export class HeaderComponent {
+  isUserLoggedIn = false;
+  isSidebarOpen = false;
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+}
